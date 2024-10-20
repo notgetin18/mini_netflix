4259c1901937e063cbe08d879a0f7c6441e1da30
Amit kumar Singh <notgetin18@gmail.com>
2024-10-20 14:11:41 +0530

    movie details page

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -45,7 +45,7 @@ const Page = () => {
       <div className={styles.movieHeader} style={{ backgroundImage: `url(${movieDetails.Poster})` }}>
         <div className={styles.overlay}>
           <div className={styles.posterWrapper}>
-            <Image  width={1680} height={450} src={movieDetails.Poster} alt={movieDetails.Title} className={styles.poster} />
+            <Image width={1680} height={450} src={movieDetails.Poster} alt={movieDetails.Title} className={styles.poster} />
           </div>
           <div className={styles.movieInfo}>
             <h2>{movieDetails.Title}</h2>
@@ -73,13 +73,19 @@ const Page = () => {
 
       <div className={styles.movieDetails}>
         <h3>Movie Info.</h3>
-        <p><strong>Actors:</strong> {movieDetails.Actors}</p>
-        <p><strong>Director:</strong> {movieDetails.Director}</p>
-        <p><strong>Country:</strong> {movieDetails.Country}</p>
-        <p><strong>Awards:</strong> {movieDetails.Awards}</p>
-        <p><strong>Box Office:</strong> {movieDetails.BoxOffice}</p>
-        <p><strong>Rated:</strong> {movieDetails.Rated}</p>
-        <p><strong>Year:</strong> {movieDetails.Year}</p>
+        <div className={styles.detailsWrapper}>
+          <div>
+            <p><strong>Actors:</strong> {movieDetails.Actors}</p>
+            <p><strong>Awards:</strong> {movieDetails.Awards}</p>
+            <p><strong>Box Office:</strong> {movieDetails.BoxOffice}</p>
+            <p><strong>Year:</strong> {movieDetails.Year}</p>
+          </div>
+          <div>
+            <p><strong>Director:</strong> {movieDetails.Director}</p>
+            <p><strong>Country:</strong> {movieDetails.Country}</p>
+            <p><strong>Rated:</strong> {movieDetails.Rated}</p>
+          </div>
+        </div>
       </div>
 
       <div className={styles.actions}>
