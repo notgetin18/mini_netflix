de1438c664436db25e0a038d1be70e87f586dcf5
Amit kumar Singh <notgetin18@gmail.com>
2024-10-20 19:00:44 +0530

    assignment completed

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   env: {
     BASE_URL: "https://www.omdbapi.com/",
-    // API_KEY: "fc1cc9bf",
-    API_KEY: "e8e9e53f",
+    API_KEY: "fc1cc9bf",
+    // API_KEY: "e8e9e53f",
   },
 
   images: {
